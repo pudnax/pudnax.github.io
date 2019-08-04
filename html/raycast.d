bdd610cf7fb21959cb812a326ec47bec041f36d6
komm <k.a.komissar@gmail.com>
2019-08-04 12:41:05 +0600

    add raycast

---
+++ html/raycast.d
@@ -0,0 +1,5 @@
+/home/komm/RustProjects/quicksilver/quicksilver-master/target/wasm32-unknown-unknown/release/examples/raycast.wasm: examples/raycast.rs
+
+/home/komm/RustProjects/quicksilver/quicksilver-master/target/wasm32-unknown-unknown/release/examples/raycast.d: examples/raycast.rs
+
+examples/raycast.rs:
