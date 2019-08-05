1e38707fff6098acc238125173b676b9958265f1
komm <k.a.komissar@gmail.com>
2019-08-05 17:35:30 +0600

    +

--- html/polar_perlin_v1.js
+++ html/polar_perlin_v1.js
@@ -10,7 +10,7 @@ if( typeof Rust === "undefined" ) {
     } else if( typeof module === "object" && module.exports ) {
         module.exports = factory();
     } else {
-        Rust.raycast_2d = factory();
+        Rust.polar_perlin_v1 = factory();
     }
 }( this, function() {
     return (function( module_factory ) {
@@ -19,13 +19,13 @@ if( typeof Rust === "undefined" ) {
         if( typeof process === "object" && typeof process.versions === "object" && typeof process.versions.node === "string" ) {
             var fs = require( "fs" );
             var path = require( "path" );
-            var wasm_path = path.join( __dirname, "raycast_2d.wasm" );
+            var wasm_path = path.join( __dirname, "polar_perlin_v1.wasm" );
             var buffer = fs.readFileSync( wasm_path );
             var mod = new WebAssembly.Module( buffer );
             var wasm_instance = new WebAssembly.Instance( mod, instance.imports );
             return instance.initialize( wasm_instance );
         } else {
-            var file = fetch( "raycast_2d.wasm", {credentials: "same-origin"} );
+            var file = fetch( "polar_perlin_v1.wasm", {credentials: "same-origin"} );
 
             var wasm_instance = ( typeof WebAssembly.instantiateStreaming === "function"
                 ? WebAssembly.instantiateStreaming( file, instance.imports )
@@ -39,11 +39,11 @@ if( typeof Rust === "undefined" ) {
             return wasm_instance
                 .then( function( wasm_instance ) {
                     var exports = instance.initialize( wasm_instance );
-                    console.log( "Finished loading Rust wasm module 'raycast_2d'" );
+                    console.log( "Finished loading Rust wasm module 'polar_perlin_v1'" );
                     return exports;
                 })
                 .catch( function( error ) {
-                    console.log( "Error loading Rust wasm module 'raycast_2d':", error );
+                    console.log( "Error loading Rust wasm module 'polar_perlin_v1':", error );
                     throw error;
                 });
         }
